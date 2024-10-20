428481a267a662d52056f31c9a6b69a4e5293fa6
unknown <gastonsalvatierra@icloud.com>
2024-10-20 17:07:17 -0300

    fix en page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,35 @@
+/* 'use client'
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [personas, setPersonas] = useState([]);
+
+  useEffect(() => {
+    const fetchPersonas = async () => {
+      const response = await fetch('/api/personas');
+      const data = await response.json();
+      setPersonas(data);
+    };
+
+    fetchPersonas();
+  }, []);
+
+  return (
+    <div>
+      <h1>Listado de Personas</h1>
+      <ul>
+        {personas.map((persona, index) => (
+          <li key={index}>
+            {persona.nombre} {persona.apellido}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+ */
+
+
 'use client';
 import React, { useState } from 'react';
 import axios from 'axios';
